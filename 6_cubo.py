3d9351e9d84ed30e15d583f034c7174344f298e7
Lizeth1800 <liset_nestor@hotmail.com>
2017-03-28 14:20:15 -0600

    se agrego suma al repositorio

--- 6_cubo.py
+++ 6_cubo.py
@@ -8,9 +8,6 @@ def seis():
         cad=cad+" "+str(C)
         suma+=C
     print(cad)
-<<<<<<< HEAD
+
     print("La suma de cubos es: ", suma)
-=======
-    print("La suma de los cubos es: ", suma)
->>>>>>> 479628175fa9e9b9b0347b2320615300cbee6f7b
 seis()
